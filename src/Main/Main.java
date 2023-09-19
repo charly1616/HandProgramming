0cf32ccf9e3a0ecf5554cc793e7f830e2ebd1867
juuzojason <juan.tobijose@gmail.com>
2023-09-19 17:57:30 -0500

    s

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,26 +1,34 @@
 
 package Main;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        
-        Parent root;
-        root = FXMLLoader.load(getClass().getResource("ventana.fxml"));
-        
-        primaryStage.setTitle("CABEZA DE BOLA");
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ventana.fxml"));
+
+        primaryStage.setTitle("PROYECT SISTEMAS");
+
+        // Obtener el tamaño de la pantalla
+        Screen pantalla = Screen.getPrimary();
+        javafx.geometry.Rectangle2D coordenadas = pantalla.getVisualBounds();
+
+        // Configurar el tamaño de la ventana para que ocupe toda la pantalla
+        primaryStage.setX(coordenadas.getMinX());
+        primaryStage.setY(coordenadas.getMinY());
+        primaryStage.setWidth(coordenadas.getWidth());
+        primaryStage.setHeight(coordenadas.getHeight());
+
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
