321cde94a175e404b4e624b66ab3af7d20c73a52
Anye1909 <144399988+Anye1909@users.noreply.github.com>
2023-09-21 23:29:06 -0500

    cambio

--- src/Bloques/BloqueInicio.java
+++ src/Bloques/BloqueInicio.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polyline;
+import javafx.scene.shape.StrokeType;
 import javafx.scene.text.Font;
 
 
@@ -19,7 +20,7 @@ public class BloqueInicio extends Bloque{
     
     public Label label;
     public Button boton;
-    public Polyline triangulo;
+    public Polyline tri;
     
     
     
@@ -42,12 +43,12 @@ public class BloqueInicio extends Bloque{
         label = new Label("Inicio");
         
         //Posicion del label
-        label.setLayoutX(0);
-        label.setLayoutY(0);
+        label.setLayoutX(62.0);
+        label.setLayoutY(15.0);
         
         //tamaño del label
-        label.setPrefHeight(50); //altura
-        label.setPrefWidth(100); //ancho
+        label.setPrefHeight(40.0); //altura
+        label.setPrefWidth(94.0); //ancho
         
         //Se coloca la fuente
         Font font = new Font("Berlin Sans FB",35);
@@ -56,7 +57,44 @@ public class BloqueInicio extends Bloque{
         //Se muestra en el bloque
         getChildren().add(label);
         
+        // Creando el botón
+        boton = new Button ();
+        
+        //Posicion del botón
+        boton.setLayoutX(25.0);
+        boton.setLayoutY(15.0);
+        
+         //tamaño del botón 
+        boton.setPrefHeight(40.0); //altura
+        boton.setPrefWidth(36.0); //ancho
+        
+        // transparencia 
+        boton.setMnemonicParsing(false);
+        
+        //
+        boton.setOpacity(0.0);
+        
+        getChildren().add(boton);
+        // creando el triangulo
+        tri = new Polyline();
+        
+         //Posicion del triangulo
+        tri.setLayoutX(49.0);
+        tri.setLayoutY(34.0);
+        
+        //
+        tri.setFill(Color.LIME);
+        //tri.points();
+      
+        tri.getPoints().addAll(-18.0, 16.0, -18.0, -13.0, 6.0, 2.0, -18.0, 16.0);
+        tri.setStroke(Color.valueOf("#2c2c2c") );
+        tri.setStrokeType(StrokeType.INSIDE);
+        tri.setStrokeWidth(5.0);
+        
+        getChildren().add(tri);
+
+        
+        
     }
-    
-    
+       
 }
