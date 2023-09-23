26e3ef1d058d3ce83af2490d585ae55ce0cca3f2
olgasp01 <olga03sp@gmail.com>
2023-09-22 23:39:06 -0500

    holi

--- src/Bloques/BloqueValor.java
+++ src/Bloques/BloqueValor.java
@@ -31,23 +31,28 @@ public class BloqueValor extends BloqueTexto{
         indicador.setText(type);
     }
     
-    
-    public String GetType(){
-        
-        return "";
+  public String GetType(String valor) {
+        String type = "";
+        if (valor.equals("")) {
+            type = "Non";
+        } else if (esNumero(valor)) {
+            type = "Num";
+        } else if (valor.equalsIgnoreCase("true") || valor.equalsIgnoreCase("false")) {
+            type = "Bol";
+        } else {
+            type = "Str";
+        }
+        return type;
     }
-    
-    
-    
-    public boolean esNumero(String n){
+
+    public boolean esNumero(String n) {
         try {
             double b = Double.parseDouble(n);
             return true;
-        }catch (Exception e){
+        } catch (NumberFormatException e) {
             return false;
         }
     }
-    
-    
+   
     
 }
