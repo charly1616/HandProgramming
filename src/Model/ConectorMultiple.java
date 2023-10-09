6420d771b025e8df16d6342eb0c633d9a4c34d45
charly1616 <124099579+charly1616@users.noreply.github.com>
2023-10-08 19:16:56 -0500

    documentacion

--- src/Model/ConectorMultiple.java
+++ src/Model/ConectorMultiple.java
@@ -11,6 +11,14 @@ public class ConectorMultiple extends Conector{
     
     public double largoConector=0;
     
+    
+    /* 
+        Recibe: (Bloque conectador) que es el bloque al que está conectado
+        Devuelve: (void) (Nada)
+        Hace: llama al constructor papa y hace que el conectador sea siempre vertical, hace que "conectador" no se pueda conectar horizontalmente
+        llama a dos funciones -({setLargoLinea}{setPreBloqueY}) para que se ajusten las posiciones del prebloque y la linea
+        
+    */
     public ConectorMultiple(Bloque conectador) {
         super(conectador, "v");
         
@@ -22,6 +30,15 @@ public class ConectorMultiple extends Conector{
 //        getChildren().add(cir);
     }
     
+    
+    
+    
+    /*  Metodo sobreescrito
+        Recibe: (Nada)
+        Devuelve: (void) (Nada)
+        Hace: llama al metodo papá y añade al conector "inner", ademas le coloca sus caracteristicas respectivas y ajusta al prebloque 
+        
+    */
     @Override
     public void iniciarComponentes(){
         super.iniciarComponentes();
@@ -37,15 +54,34 @@ public class ConectorMultiple extends Conector{
     
     
     
+    /* 
+        Recibe: (Nada)
+        Devuelve: (void) (Nada)
+        Hace: establece el largo de la "linea", la linea es la punteada negra | que se muestra
+    */
     public final void setLargoLinea(){
         linea.setEndY(altoLinea());
     }
     
+    
+    
+    /* 
+        Recibe: (Nada)
+        Devuelve: (double) el largo que se cacula
+        Hace: devuelve cual es el largo de la linea
+    */
     public double altoLinea(){
         return (Bloque.ALTO-18)*(largoConector)+80;
     }
     
     
+    
+    /* 
+        Recibe: (Nada)
+        Devuelve: (void) (Nada)
+        Hace: establece la posicion Y de los rectangulos que representan al prebloque, esta depende del valor -(Metodo {altoLinea})
+        se usa porque la linea | está cambiando de tamaño constantemente y hay que hacer que el prebloque esté al final de la linea
+    */
     public final void setPreBloqueY(){
         this.SidePart.setY(-27 + altoLinea());
         this.TopPart.setY( -7 + altoLinea());
@@ -53,6 +89,12 @@ public class ConectorMultiple extends Conector{
     
     
     
+    /* Metodo que se sobreescribe porque hay que cambiar tambien la posicion del "inner"
+        Recibe: (double x,y) que son las posiciones donde se va a poner el conector
+        Devuelve: (void) (Nada)
+        Hace: establece la posicion Y de los rectangulos que representan al prebloque, esta depende del valor -(Metodo {altoLinea})
+        se usa porque la linea | está cambiando de tamaño constantemente y hay que hacer que el prebloque esté al final de la linea
+    */
     @Override
     public void setPosicion(double x, double y){
         setLayoutX(x);
@@ -65,7 +107,12 @@ public class ConectorMultiple extends Conector{
     
     
     
-    
+    /* Metodo que se sobreescribe porque hay que ajustar tambien la posicion del "inner"
+        Recibe: (Nada)
+        Devuelve: (void) (Nada)
+        Hace: se coloca al lado de su bloque "conectador" y coloca a "inner" al lado de la linea
+        como los bloques se cambian de posicion, hay que mover a sus conectores tambien
+    */
     @Override
     public void fixPosicion(){
         setLargoLinea();
@@ -88,35 +135,23 @@ public class ConectorMultiple extends Conector{
     
     
     
-    
-//    @Override
-//    public void fixLargoLineaIdentada(){
-//        if (conectador == null && conexion != null){
-//            int c = 0;
-//            
-//            if (inner.conexion != null){
-//                c = inner.conexion.LargoConexionMultiple()+1;
-//            }
-//            
-//            multiconectador.largoConector = conexion.LargoConexionMultiple()+ c;
-//            multiconectador.fixPosicion();
-//            multiconectador.fixLargoLineaIdentada();
-//            if (multiconectador.conexion != null && multiconectador.conexion.conectado != null){
-//                multiconectador.conexion.conectado.fixLargoLineaIdentada();
-//            }
-//        }
-//        if (conectador != null && conectador.conectado != null){
-//            conectador.conectado.fixLargoLineaIdentada();
-//        }
-//    }
-//    
-    
+    /* Metodo que se sobreescribe porque hay que evitar que se pueda ocultar la linea, no es necesario para el conectador Multiple
+        Recibe: (Nada)
+        Devuelve: (void) (Nada)
+        Hace: (Nada)
+    */
     @Override
     public void ocultarLinea(){
     }
     
     
     
+    
+    /* Metodo que se sobreescribe porque los vertices de la conexion dependen del largo de la linea |, los vertices van a estár al final de la linea
+        Recibe: (Nada)
+        Devuelve: (double[]) un array con los puntos de los vertices del conector multiple
+        Hace: calcula los puntos de los vertices del conector multiple dependiendo de la altura y lo devuelve
+    */
     @Override
     public double[] getRectVertices(){
         double [] d = {getX()+7, getY()+altoLinea(), getX()+conectador.ancho/2.0, getY()+(Bloque.ALTO-15)/2.0+altoLinea()};
