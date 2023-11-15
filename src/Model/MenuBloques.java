d3ef545a853ccea1861a64f26217be8aee7be083
juuzojason <juan.tobijose@gmail.com>
2023-11-14 23:10:01 -0500

    m

--- src/Model/MenuBloques.java
+++ src/Model/MenuBloques.java
@@ -36,21 +36,21 @@ public class MenuBloques extends VBox{
 
         
           // Botón 9
-        Button button9 = createButton("/src/Images/Open 0Box.png", "#EA4E00");
+            Button button9 = createButton("/Images/For.png", "#EA4E00");
         getChildren().add(button9);
         button9.setOnAction(e -> {
             crearFor();
         });
         
         // Botón 1
-        Button button1 = createButton("/src/Images/Open 0Box.png", "#FFA917");
+        Button button1 = createButton("/Images/Box.png", "#FFA917");
         getChildren().add(button1);
         button1.setOnAction(e -> {
             crearVar();
         });
 
         // Botón 2
-        Button button2 = createButton("@../../AppData/Local/Temp/Icons8/Pichon/Multiply.png", "#FFE333");
+        Button button2 = createButton("/Images/Tags_3.png", "#F3FD8B");
         getChildren().add(button2);
         button2.setOnAction(e -> {
             crearDat();
@@ -63,28 +63,28 @@ public class MenuBloques extends VBox{
 
 
         // Botón 3
-        Button button3 = createButton("@../../AppData/Local/Temp/Icons8/Pichon/Repeat_2.png", "#A77CE0");
+        Button button3 = createButton("/Images/Repeat.png", "#A77CE0");
         getChildren().add(button3);
         button3.setOnAction(e -> {
             crearWhile();
         });
 
         // Botón 4
-        Button button4 = createButton("@../../AppData/Local/Temp/Icons8/Pichon/Share_1.png", "#88DBFF");
+        Button button4 = createButton("/Images/if.png", "#88DBFF");
         getChildren().add(button4);
         button4.setOnAction(e -> {
             crearIf();
         });
         
         // Botón 0
-        Button button0 = createButton("@../../AppData/Local/Temp/Icons8/Pichon/Share_1.png", "#0DF7A9");
+        Button button0 = createButton("/Images/Else.png", "#0DF7A9");
         getChildren().add(button0);
         button0.setOnAction(e -> {
             crearElse();
         });
         
         // Botón 01
-        Button button01 = createButton("@../../AppData/Local/Temp/Icons8/Pichon/Share_1.png", "#01D527");
+        Button button01 = createButton("/Images/Multicast.png", "#01D527");
         getChildren().add(button01);
         button01.setOnAction(e -> {
             crearElif();
@@ -96,17 +96,17 @@ public class MenuBloques extends VBox{
         getChildren().add(separator3);
 
         // Botón 5
-        Button button5 = createButton("@../../AppData/Local/Temp/Icons8/Pichon/Export.png", "#FF6D87");
+        Button button5 = createButton("/Images/Next.png", "#FF6D87");
         getChildren().add(button5);
         button5.setOnAction(e -> {
             crearMostrar();
         });
 
         // Botón 6
-        Button button6 = createButton("@../../AppData/Local/Temp/Icons8/Pichon/Input.png", "#7EAA00");
+        Button button6 = createButton("/Images/Input.png", "#7EAA00");
         getChildren().add(button6);
         button6.setOnAction(e -> {
-            System.out.println("Pediurrrrr");
+            System.out.println("Pedir");
             crearPedir();
         });
         
