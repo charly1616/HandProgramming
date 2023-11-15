62f2bcf32da45d6cdb7f29bb2dc7c2b58ce1132f
juuzojason <juan.tobijose@gmail.com>
2023-11-14 23:36:44 -0500

    m

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,30 +1,22 @@
 package Main;
 
-import Model.EvaluadorExpresiones;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Main extends Application {
-    
-    
-    
+
+    private static Process pythonProcess;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Crear una escena con StackPane para superponer el fondo
@@ -45,7 +37,7 @@ public class Main extends Application {
         Image backgroundImage = new Image("2.jpg");
         ImageView backgroundImageView = new ImageView(backgroundImage);
         rootPane.getChildren().add(backgroundImageView);
-        
+
         primaryStage.show();
 
         // Simular la carga gradual con una animación
@@ -66,10 +58,16 @@ public class Main extends Application {
                     }
                 })
         );
-        
-        
+
         timeline.setCycleCount(1); // Ejecutar una vez
         timeline.play();
+    }
+
+    @Override
+    public void stop() {
+        if (pythonProcess != null) {
+            pythonProcess.destroy();
+        }
     }
 
     public static void main(String[] args) {
@@ -79,39 +77,16 @@ public class Main extends Application {
 
             // Comando para ejecutar el script de Python
             String[] command = {"C:/Users/juand/AppData/Local/Microsoft/WindowsApps/python.exe", scriptPath};   //Juanda
-//            String[] command = {"C:/Users/User/AppData/Local/Programs/Python/Python311/python.exe", scriptPath};   //Charly
-            
+//          String[] command = {"C:/Users/User/AppData/Local/Programs/Python/Python311/python.exe", scriptPath};   //Charly
+
             // Crear el proceso
             ProcessBuilder processBuilder = new ProcessBuilder(command);
-            Process process = processBuilder.start();
-            
-
-//            // Leer la salida estándar del proceso
-//            InputStream inputStream = process.getInputStream();
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-//
-//            // Leer la salida de error del proceso
-//            InputStream errorStream = process.getErrorStream();
-//            BufferedReader errorReader = new BufferedReader(new InputStreamReader(errorStream));
-
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                System.out.println(line);
-//            }
-//
-//            // Imprimir la salida de error
-//            while ((line = errorReader.readLine()) != null) {
-//                System.err.println(line);
-//            }
-
-            // Esperar a que el proceso termine
-//            int exitCode = process.waitFor();
-//            System.out.println("El script de Python ha terminado con código de salida: " + exitCode);
+            pythonProcess = processBuilder.start();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         launch(args);
     }
 }
